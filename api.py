7543b62a7f48a5921b3b2bc48f72452683ff5f2a
kayhoogland <kay@floryn.com>
2021-10-15 18:12:07 +0200

    First commit

---
+++ api.py
@@ -0,0 +1,70 @@
+from typing import Optional
+import os
+from dotenv import load_dotenv
+
+from fastapi import FastAPI
+
+from src import scrape
+
+app = FastAPI()
+
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
+class Date(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    kind: str
+    date: str
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def delete_all():
+    with Session(engine) as session:
+        session.query(Date).delete()
+        session.commit()
+
+
+def fill_table():
+    postal_code = os.getenv("POSTAL_CODE")
+    number = os.getenv("NUMBER")
+    scraper = scrape.Scrape(postal_code, number)
+    with Session(engine) as session:
+        for k, dates in scraper.reminder_dates_for_all_kinds.items():
+            for d in dates:
+                session.add(Date(kind=k, date=d))
+        session.commit()
+    return True
+
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    load_dotenv()
+    create_db_and_tables()
+    delete_all()
+    fill_table()
+
+
+@app.get("/dates")
+def all_dates():
+    with Session(engine) as session:
+        dates = session.exec(select(Date)).all()
+        return dates
+
+
+@app.get("/dates/{date}")
+def date(date: str):
+    with Session(engine) as session:
+        return session.exec(select(Date).where(Date.date == date)).first()
---
+++ src/scrape.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+
+import pandas as pd
+from gazpacho import Soup, get
+from functools import cached_property
+
+URL = "https://afvalstoffendienstkalender.nl/nl/{postal_code}/{number}"
+MONTHS = [
+    "januari",
+    "februari",
+    "maart",
+    "april",
+    "mei",
+    "juni",
+    "juli",
+    "augustus",
+    "september",
+    "oktober",
+    "november",
+    "december",
+]
+MONTH_MAP = {m: str(i).rjust(2, "0") for m, i in zip(MONTHS, range(1, len(MONTHS) + 1))}
+ALLOWED_KINDS = ["papier", "gft", "restafval"]
+
+
+def current_year():
+    return datetime.now().year
+
+
+class Scrape:
+    def __init__(self, postal_code: str, number: int):
+        self.postal_code = postal_code
+        self.number = number
+
+    @cached_property
+    def soup(self):
+        html = get(URL.format(postal_code=self.postal_code, number=self.number))
+        return Soup(html)
+
+    @cached_property
+    def reminder_dates_for_all_kinds(self):
+        return {k: self.reminder_dates_for_kind(k) for k in ALLOWED_KINDS}
+
+    def dates_for_kind(self, kind: str):
+        return set(
+            p.text
+            for p in self.soup.find("p", {"class": kind}, partial=False)
+            if p.text not in [kind, "vandaag"]
+        )
+
+    def reminder_dates(self, dates):
+        return sorted([self.build_reminder_date(d) for d in dates])
+
+    def reminder_dates_for_kind(self, kind: str):
+        return self.reminder_dates(self.dates_for_kind(kind))
+
+    @staticmethod
+    def build_reminder_date(date):
+        _, day, month = date.split(" ")
+        month_number = MONTH_MAP[month]
+        date = pd.to_datetime((f"{current_year()}-{month_number}-{day.rjust(2, '0')}"))
+        day_before = (date - pd.Timedelta(days=1)).strftime("%Y-%m-%d")
+        return day_before
